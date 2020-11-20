e891b22a0da8da7e235e6eb2d9c5ba4af84a5c88
lxf_boy@163.com <lxf_boy@163.com>
2020-11-20 09:25:32 +0800

    add project

---
+++ src/test/java/cn/ryen/civilization/CivilizationApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.ryen.civilization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CivilizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
